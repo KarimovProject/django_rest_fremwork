d89672ab14b1959a046e916d39d9cd0c2ee823d9
KarimovProject <abdullatifkarimov772@gmail.com>
2022-11-15 19:21:38 +0500

    url is ready

--- api/models.py
+++ api/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    color = models.CharField(max_length=100)
+    ram = models.IntegerField()
+    memory = models.IntegerField()
+    price = models.FloatField()
+    image = models.CharField(max_length=100)
+    release_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
