6d5efb6e07c9639edf9ec5c7ace609211dcc926c
KarimovProject <abdullatifkarimov772@gmail.com>
2022-11-15 19:09:06 +0500

    views is ready

--- api/views.py
+++ api/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+
+from rest_framework.request import Request
+
+@api_view(['GET'])
+def home(request:Request):
+    data = request.query_params
+
+    return Response({'result': data})
